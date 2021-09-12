5c74f209af9b04c2b3d898798d555ecbc4dbc50d
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-09-12 10:39:12 +0200

    Add empty project

---
+++ src/Domain/IBankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public interface IBankAccount
+    {
+        #region Properties
+        public string AccountNumber { get; }
+        public decimal Balance { get; }
+        public int NumberOfTransactions { get; }
+        #endregion
+
+        #region Methods
+        void Deposit(decimal amount);
+        void Withdraw(decimal amount);
+        IEnumerable<Transaction> GetTransactions(DateTime? from, DateTime? till);
+        #endregion
+    }
+}
---
+++ src/Domain/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Transaction
+    {
+        #region Properties
+        public decimal Amount { get; }
+        public DateTime DateOfTrans { get; }
+        public bool IsDeposit => TransactionType == TransactionType.Deposit;
+        public bool IsWithdraw => TransactionType == TransactionType.Withdraw;
+        public TransactionType TransactionType { get; }
+        #endregion
+
+        #region Constructors
+        public Transaction(decimal amount, TransactionType type)
+        {
+            Amount = amount;
+            TransactionType = type;
+        }
+        #endregion
+    }
+}
